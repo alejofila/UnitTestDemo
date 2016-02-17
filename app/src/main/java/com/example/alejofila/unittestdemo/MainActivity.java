c6b35ffd9d985688f6b04b423995481c68947e72
alejofilaKoombea <alejofila@hotmail.com>
2016-02-17 17:35:37 -0500

    First tesd added

--- app/src/main/java/com/example/alejofila/unittestdemo/MainActivity.java
+++ app/src/main/java/com/example/alejofila/unittestdemo/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.alejofila.unittestdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText text;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (EditText) findViewById(R.id.txt1)
+        ;
+
+    }
+
+
+    public void onCalculateButtonClicked(View view) {
+        RadioButton celsiusButton = (RadioButton) findViewById(R.id.radio0);
+        RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.radio1);
+        if (text.getText().length() == 0) {
+            Toast.makeText(this, "Please enter a valid number",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        float inputValue = Float.parseFloat(text.getText().toString());
+        if (celsiusButton.isChecked()) {
+            text.setText(String
+                    .valueOf(ConverterUtil.convertFahrenheitToCelsius(inputValue)));
+            celsiusButton.setChecked(false);
+            fahrenheitButton.setChecked(true);
+        } else {
+            text.setText(String
+                    .valueOf(ConverterUtil.convertCelsiusToFahrenheit(inputValue)));
+            fahrenheitButton.setChecked(false);
+            celsiusButton.setChecked(true);
+        }
+
+
     }
 }
+
